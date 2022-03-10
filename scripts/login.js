9c61d2de15148fd251143c21b0add6654251b20f
Spacenin <chepstudios@gmail.com>
2022-03-10 17:40:03 -0500

    Added fleshed out css design for login page

---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+function getLoginCreds() {
+    var username = document.getElementById("usernameBox").value;
+    var password = document.getElementById("passwordBox").value;
+
+    console.log(username, password);
+}
