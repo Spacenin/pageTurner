4cd9c23dbd3ec6695bd544f7062f333abc7f886d
Spacenin <chepstudios@gmail.com>
2022-03-11 21:12:48 -0500

    Testing php

--- scripts/login.js
+++
@@ -1,6 +0,0 @@
-function getLoginCreds() {
-    var username = document.getElementById("usernameBox").value;
-    var password = document.getElementById("passwordBox").value;
-
-    console.log(username, password);
-}
